9e062b5f47e4d70f9358df9f443031d83c974390
Tim <timothy.b.stott@gmail.com>
2022-09-16 20:37:13 -0400

    Added background and size change functionality

--- main.js
+++ main.js
@@ -12,6 +12,65 @@ function addSquareHoveredClassToDiv(e){
      this.classList.add("squareHovered");
 }
 
+
 //add hover event listener
+function addHoverEventListener(){
 const hoverableDivs = document.querySelectorAll(".square")
 hoverableDivs.forEach(div => div.addEventListener('mouseover', addSquareHoveredClassToDiv));
+}
+
+//add eventlistenter for size buttons
+const sizeBtn = document.querySelectorAll(".sizeButton")
+sizeBtn.forEach(btn => btn.addEventListener('click', changeNumberOfDivs));
+
+//function for changing number of divs.
+function changeNumberOfDivs (e){
+    mainContainer.innerHTML = "";
+    switch(e.target.id)
+    {
+        case "16x16":
+            for(let i =0; i<(16*16);i++)
+            {
+                var squares = document.createElement("div");
+                squares.classList.add("square");
+                mainContainer.style.gridTemplateColumns = "repeat(16,auto)";
+                mainContainer.appendChild(squares);
+            }
+            break;
+            
+        case "32x32":
+
+            for(let i =0; i<(32*32);i++)
+            {
+                var squares = document.createElement("div");
+                squares.classList.add("square");
+                mainContainer.style.gridTemplateColumns = "repeat(32,auto)";
+                mainContainer.appendChild(squares);
+            }
+            break;
+        
+        case "64x64":
+            for (let i = 0; i<(64*64);i++)
+            {
+                var squares = document.createElement("div");
+                squares.classList.add("square");
+                mainContainer.style.gridTemplateColumns = "repeat(64,auto)";
+                mainContainer.appendChild(squares);
+            }
+            break;
+    }
+
+    addHoverEventListener();
+
+}
+
+//function for clearing div
+function removeAllChildren(divToRemoveFrom){
+    while(divToRemoveFrom.firstChild){
+        divToRemoveFrom.removeChild(divToRemoveFrom.firstChild);
+    }
+}
+
+
+addHoverEventListener();
+
