2cbdb9b7a1ea39f112807e28680b14cae9fcfe17
Tim <timothy.b.stott@gmail.com>
2022-09-17 10:18:23 -0400

    Add Background and some formatting plus clear logic

--- main.js
+++ main.js
@@ -7,6 +7,14 @@ for(let i =0; i<(16*16);i++){
     mainContainer.appendChild(square);
 }
 
+//add eventlistenter for size buttons
+const sizeBtn = document.querySelectorAll(".sizeButton")
+sizeBtn.forEach(btn => btn.addEventListener('click', changeNumberOfDivs));
+
+//add event listener for clear button
+const clearBtn = document.querySelector("#clearButton");
+clearBtn.addEventListener('click', clearBlackFromSquares);
+
 //add squareHovered class
 function addSquareHoveredClassToDiv(e){
      this.classList.add("squareHovered");
@@ -19,9 +27,6 @@ const hoverableDivs = document.querySelectorAll(".square")
 hoverableDivs.forEach(div => div.addEventListener('mouseover', addSquareHoveredClassToDiv));
 }
 
-//add eventlistenter for size buttons
-const sizeBtn = document.querySelectorAll(".sizeButton")
-sizeBtn.forEach(btn => btn.addEventListener('click', changeNumberOfDivs));
 
 //function for changing number of divs.
 function changeNumberOfDivs (e){
@@ -71,6 +76,13 @@ function removeAllChildren(divToRemoveFrom){
     }
 }
 
-
+//Runs to initialy add the event listener
 addHoverEventListener();
+
+//function to clear background
+function clearBlackFromSquares(){
+    const blackSquares = document.querySelectorAll(".squareHovered");
+    blackSquares.forEach(sqr => sqr.classList.remove("squareHovered")); 
+    addHoverEventListener();
+}
 
